de41208393b19fd5fee569100c60b625efec0aab
Choi Gaeun <119295401+eunii2@users.noreply.github.com>
2024-09-25 21:28:23 +0900

    [Chore] 오타 수정?

--- Back-end/src/main/java/org/backend/Controller/ConferenceController.java
+++ Back-end/src/main/java/org/backend/Controller/ConferenceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8000")
+@CrossOrigin(origins = "http://localhost:8080")
 public class ConferenceController {
 
     @Autowired
